f3f169851993f3b2f6e342a28d8a6809154bb5ae
Jaanus Siim <jaanus@jaanussiim.com>
2014-05-29 21:18:38 +0300

    Initial project files

---
+++ CDYImageScale/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CDYImageScale
+//
+//  Created by Jaanus Siim on 29/05/14.
+//  Copyright (c) 2014 Coodly LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CDYImageScale/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CDYImageScale
+//
+//  Created by Jaanus Siim on 29/05/14.
+//  Copyright (c) 2014 Coodly LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
